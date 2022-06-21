e99e16f4cd953bec08dc62427eafefc1e1d6bf76
Abel Jansen <abel@swiftcom.co.za>
2022-06-21 15:37:04 +0200

    chore: add category tree nest limit

--- packages/composables/src/getters/categoryGetters.ts
+++ packages/composables/src/getters/categoryGetters.ts
@@ -4,6 +4,8 @@ import type { Category } from '@vue-storefront/woocommerce-api';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function getTree(categories: Category[], currentCategory: string): AgnosticCategoryTree {
+
+  // Build hashtable used to construct category tree
   const hashTable = Object.create(null);
   categories.forEach(category => hashTable[category.id] = {
     label: category.title,
@@ -14,8 +16,8 @@ function getTree(categories: Category[], currentCategory: string): AgnosticCateg
     count: category.count
   });
 
+  // Construct category tree
   const categoryTree = [];
-
   categories.forEach(category => {
     if (category.parent_id && hashTable[category.parent_id]) {
       hashTable[category.parent_id].items.push(hashTable[category.id]);
@@ -24,7 +26,8 @@ function getTree(categories: Category[], currentCategory: string): AgnosticCateg
     }
   });
 
-  let selectedTree = {
+  // The tree root that will be returned
+  let categoryTreeRoot = {
     label: '',
     slug: '',
     slugPath: '',
@@ -34,12 +37,26 @@ function getTree(categories: Category[], currentCategory: string): AgnosticCateg
 
   const path = currentCategory.split('/').filter((item) => Boolean(item));
 
+  // Calculate what the root of the tree should be.
+  // The root should be the parent node of the currently selected category
+  // so that the current category is a dropdown in the list.
+  // The root shoud be higher than the parent if the currently displayed categories
+  // have no children, i.e. we cannot go deeper so that there is always useful information.
   path.every((item) => {
-    for (let i = 0; i < selectedTree.items.length; i++) {
-      if (selectedTree.items[i].slug === `${item}/` || selectedTree.items[i].slug === item) {
-        selectedTree = selectedTree.items[i];
+    for (let i = 0; i < categoryTreeRoot.items.length; i++) {
+      if (categoryTreeRoot.items[i].slug === `${item}/` || categoryTreeRoot.items[i].slug === item) {
 
-        if (selectedTree.items.some((item) => item.slugPath === currentCategory)) {
+        const previousTree = categoryTreeRoot;
+
+        categoryTreeRoot = categoryTreeRoot.items[i];
+
+        // If the currently selected root's child is the selected category, but it has no children
+        // then the root should be higher up.
+        if (categoryTreeRoot.items.find((item) => item.slugPath === currentCategory)?.items?.length === 0 && previousTree.label) {
+          categoryTreeRoot = previousTree;
+        }
+
+        if (categoryTreeRoot.items.some((item) => item.slugPath === currentCategory)) {
           return false;
         }
       }
@@ -48,7 +65,7 @@ function getTree(categories: Category[], currentCategory: string): AgnosticCateg
     return true;
   });
 
-  return selectedTree;
+  return categoryTreeRoot;
 }
 
 function getName(category: Category): string {
